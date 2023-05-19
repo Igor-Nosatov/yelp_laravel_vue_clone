0e17cfebeef4e5e6f4432cd27fdd828404bc6dca
Igor_Nosatov <nosatov_ik@groupbwt.com>
2023-05-19 07:38:02 +0300

    feat:add home page

--- resources/js/layouts/App.vue
+++ resources/js/layouts/App.vue
@@ -1,68 +1,59 @@
 <template>
-  <v-layout>
-    <v-container>
-      <v-app-bar>
-        <v-row>
-          <v-col cols="2">
-            <v-app-bar-nav-icon>
-              <img alt="Yelp" />
-            </v-app-bar-nav-icon>
-          </v-col>
-          <v-col cols="2">
-            <v-select
-              label="Select"
-              :items="[
-                'California',
-                'Colorado',
-                'Florida',
-                'Georgia',
-                'Texas',
-                'Wyoming',
-              ]"
-            ></v-select>
-          </v-col>
-          <v-col cols="2">
-            <v-select
-              label="Select"
-              :items="[
-                'California',
-                'Colorado',
-                'Florida',
-                'Georgia',
-                'Texas',
-                'Wyoming',
-              ]"
-            ></v-select>
-          </v-col>
-          <v-col cols="1">
-            <v-select
-              label="Select"
-              :items="[
-                'California',
-                'Colorado',
-                'Florida',
-                'Georgia',
-                'Texas',
-                'Wyoming',
-              ]"
-            ></v-select>
-          </v-col>
-          <v-col cols="3">
-            <v-btn text>Write Review</v-btn>
-            <v-btn outlined>Login</v-btn>
-            <v-btn elevated>Sign Up</v-btn>
-          </v-col>
-        </v-row>
-      </v-app-bar>
-      <v-row class="router-view">
-        <v-col cols="1"></v-col>
-        <v-col cols="10"><router-view /></v-col>
-        <v-col cols="1"></v-col>
-      </v-row>
-      <TopFooter />
+    <v-layout>
+        <v-container>
+            <v-app-bar>
+                <v-row>
+                    <v-col cols="2">
+                        <v-app-bar-nav-icon>
+                            <img alt="Yelp" />
+                        </v-app-bar-nav-icon>
+                    </v-col>
+                    <v-col cols="2">
+                        <v-select label="Select" :items="[
+                            'California',
+                            'Colorado',
+                            'Florida',
+                            'Georgia',
+                            'Texas',
+                            'Wyoming',
+                        ]"></v-select>
+                    </v-col>
+                    <v-col cols="2">
+                        <v-select label="Select" :items="[
+                            'California',
+                            'Colorado',
+                            'Florida',
+                            'Georgia',
+                            'Texas',
+                            'Wyoming',
+                        ]"></v-select>
+                    </v-col>
+                    <v-col cols="1">
+                        <v-select label="Select" :items="[
+                            'California',
+                            'Colorado',
+                            'Florida',
+                            'Georgia',
+                            'Texas',
+                            'Wyoming',
+                        ]"></v-select>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-btn text>Write Review</v-btn>
+                        <v-btn outlined>Login</v-btn>
+                        <v-btn elevated>Sign Up</v-btn>
+                    </v-col>
+                </v-row>
+            </v-app-bar>
+            <v-row class="router-view">
+                <v-col cols="1"></v-col>
+                <v-col cols="10"><router-view /></v-col>
+                <v-col cols="1"></v-col>
+            </v-row>
+            <TopFooter />
             <BottomFooter />
-    </v-container>
-  </v-layout>
+        </v-container>
+    </v-layout>
 </template>
 
 <script>
@@ -70,16 +61,17 @@ import TopFooter from "@/components/footer/TopFooter.vue";
 import BottomFooter from "@/components/footer/BottomFooter.vue";
 
 export default {
-  components: {
-    TopFooter,
-    BottomFooter,
-  },
-  setup() {},
+    components: {
+        TopFooter,
+        BottomFooter,
+    },
+    setup() { },
 };
 </script>
 <style scoped>
-.router-view{
+.router-view {
     margin-top: 100px;
     margin-bottom: 100px;
+    min-height: 400px;
 }
 </style>
--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -1,8 +1,40 @@
 <template>
-    <div>
-<h1>Login</h1>
-    </div>
-</template>
-<script>
-</script>
-<style scoped></style>
+    <h1>SignUp</h1>
+    <v-sheet width="600" class="mx-auto">
+      <v-form @submit.prevent>
+        <v-text-field v-model="form.email" label="E-mail" type="email" required></v-text-field>
+        <v-text-field v-model="form.password" label="Password" type="password"></v-text-field>
+        <v-btn type="submit" block class="mt-2 signup-button">Submit</v-btn>
+      </v-form>
+    </v-sheet>
+  </template>
+
+  <script>
+  import { ref } from 'vue';
+
+  export default {
+    setup() {
+      const form = ref({
+        email: '',
+        password: ''
+      });
+
+      const rules = []; // Add your validation rules here
+
+      return {
+        form,
+        rules
+      };
+    }
+  };
+  </script>
+
+  <style scoped>
+  .signup-button {
+    background-color: #be2020;
+    height: 45px !important;
+    color: rgb(245, 243, 243);
+    font-weight: bold;
+    font-size: 16px;
+  }
+  </style>
