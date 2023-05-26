d98e3998775a5934e0f01351f20dbb31e3db945e
Igor_Nosatov <nosatov_ik@groupbwt.com>
2023-05-26 21:50:39 +0300

    finish blogitem page

--- resources/js/views/BlogItem.vue
+++ resources/js/views/BlogItem.vue
@@ -3,7 +3,7 @@
     <v-col cols="6">
       <v-row>
         <v-col cols="12" class="pt-12 pb-10">
-          <a href="" class="news-title2 tex-left pl-8">NEWS</a>
+          <a href="" class="news-title2 tex-left text-h5">NEWS</a>
         </v-col>
         <v-col cols="12">
           <h2 class="blog-item-title">
@@ -14,17 +14,13 @@
         </v-col>
         <v-col cols="12">
           <v-divider></v-divider>
-          <v-row class="pt-12 pb-12">
+          <v-row class="pt-10 pb-8">
             <v-col cols="1">
-
-            <img src="./../images/logo.png" alt="" height="50" width="120">
+              <img src="./../images/logo.png" alt="" height="50" width="120" />
             </v-col>
-            <v-col cols="8">
-
-
-            </v-col>
+            <v-col cols="8"> </v-col>
             <v-col cols="3">
-              <div class="d-flex flex-row justify-space-between">
+              <div class="d-flex flex-row justify-space-between pt-3">
                 <v-icon size="x-large" class="feedback-icon"
                   >mdi-facebook</v-icon
                 >
@@ -73,17 +69,79 @@
             and help us reach and seat more diners.”
           </p>
         </v-col>
-        <v-col cols="12"></v-col>
+        <v-col cols="12">
+
+            <v-divider></v-divider>
+            <h4 class="text-center text-h4 pt-6 pt-10 font-weight-bold">
+                Did you find this post valuable?
+            </h4>
+            <p class="text-center text-body-1  pt-4">
+                Click on a star to rate it.
+            </p>
+            <div class="d-flex flex-row justify-center   pt-2 pb-10">
+                <v-icon size="x-large" class="feedback-icon1">mdi-star-outline</v-icon>
+                <v-icon size="x-large" class="feedback-icon1">mdi-star-outline</v-icon>
+                <v-icon size="x-large" class="feedback-icon1">mdi-star-outline</v-icon>
+                <v-icon size="x-large" class="feedback-icon1">mdi-star-outline</v-icon>
+                <v-icon size="x-large" class="feedback-icon1">mdi-star-outline</v-icon>
+              </div>
+            <v-divider></v-divider>
+            <p class="text-justify  pt-10 pb-10 font-weight-thin">
+                The information above is provided for educational and informational purposes only. It is not intended to be a substitute
+                for professional advice and may not be suitable for your circumstances.
+                Unless stated otherwise, references to third-party links, services, or products do not constitute endorsement by Yelp.
+            </p>
+            <v-divider></v-divider>
+        </v-col>
       </v-row>
     </v-col>
   </v-row>
   <v-row justify="center">
     <v-col cols="8">
       <v-row>
-        <v-col cols="12"> </v-col>
+        <v-col cols="12" class="pt-8 ">
+            <div class="subscribe-block rounded-lg pt-8 pb-8">
+              <h3 class="text-center font-weight-bold subscribe-title">
+                Business resources, delivered to your inbox
+              </h3>
+              <h3
+                class="text-center font-weight-light subscribe-subtitle pb-6"
+              >
+                Get the latest blog content, info on virtual events, and the
+                occasional freebie.
+              </h3>
 
+              <v-form validate-on="submit lazy" @submit.prevent="submit" class="pt-4">
+                <v-row justify="center">
+                    <v-col cols="3">
+                        <v-text-field
+                        v-model="userEmail"
+                        :rules="rules"
+                        label="youremail@address.com"
+                        class="rounded-2 input-subscribe"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="2">
+                        <v-btn
+                        :loading="loading"
+                        type="submit"
+                        block
+                        class="rounded-2 btn-subscribe"
+                        text="Submit"
+                      ></v-btn>
+                    </v-col>
+                </v-row>
+              </v-form>
+              <v-row  justify="center">
+                <v-col cols="5">
+                    <p class="subscribe-text pt-5">By continuing, you agree to Yelp’s Terms of Service and acknowledge</p>
+                    <p  class=" subscribe-text pt-2">Yelp’s Privacy Policy.</p>
+                </v-col>
+              </v-row>
+            </div>
+          </v-col>
         <v-col cols="12">
-          <h3 class="text-left font-weight-black text-h3 pt-6 pb-6">
+          <h3 class="text-left font-weight-black text-h3 pl-7 pt-6 pb-6">
             You may also like
           </h3>
           <v-row>
@@ -246,5 +304,42 @@
 .blog-text-date {
   color: #c8cacc;
   font-weight: 500;
+}
+
+.subscribe-block {
+  background: #2b273c;
+  height: 500px;
+}
+.subscribe-title {
+  color: #fff;
+  padding: 40px 120px;
+  font-size:40px;
+}
+
+.subscribe-subtitle {
+  color: #d6d1d1;
+  padding: 5px 80px;
+  font-size:25px;
+}
+.btn-subscribe{
+    height: 50px !important;
+    width: 100px !important;
+    background-color: #f73d3d;
+    color: #fff;
+    font-weight:bold;
+}
+.input-subscribe{
+background-color: #fff !important;
+height: 50px !important;
+border-radius: 3px;
+}
+.subscribe-text{
+    color: #d6d1d1;
+    font-size: 14px;
+}
+
+.feedback-icon1{
+    color:#eaec56;
+    font-size:80px;
 }
 </style>
